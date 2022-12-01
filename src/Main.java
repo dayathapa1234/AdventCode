43993812f7ae4803cf81288843227a04707ab44c
dayathapa1234 <dayathapa1234@gmail.com>
2022-12-01 19:31:25 +0000

    Day One Completed

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        question1();
+        //dayOneQuestionOne();
+        //dayOneQuestionTwo();
     }
 
-    public static void question1(){
+    public static void dayOneQuestionOne(){
         try {
             File myObj = new File("C:\\Users\\dayat\\IdeaProjects\\AdventCalender\\src\\data.txt");
             Scanner myReader = new Scanner(myObj);
@@ -28,6 +29,36 @@ public class Main {
                 }
             }
             System.out.println(maximum);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void dayOneQuestionTwo(){
+        try {
+            File myObj = new File("C:\\Users\\dayat\\IdeaProjects\\AdventCalender\\src\\data.txt");
+            Scanner myReader = new Scanner(myObj);
+            List<Long> answers = new ArrayList<>();
+            Long current = 0l;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                if (!data.isEmpty()){
+                    current += Integer.parseInt(data);
+                }
+                if (data.isEmpty()) {
+                    answers.add(current);
+                    current = 0l;
+                }
+            }
+            //answers = Collections.sort(answers);
+            answers.sort((a1, a2) -> a2.compareTo(a1));
+            long total = 0l;
+            for (int i=0; i<=2; i++){
+                total += answers.get(i);
+            }
+            System.out.println(total);
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
